3287e9e482883a456082652cf2e07d9dca7949cb
xingjianzhang <xingjian.zhang@qq.com>
2025-05-25 08:46:47 +0000

    Add token metrics for all MoE losses

--- megatron/core/transformer/moe/router.py
+++ megatron/core/transformer/moe/router.py
@@ -187,6 +187,15 @@ class TopKRouter(Router):
             _, indices = torch.topk(logits, k=self.topk, dim=1)
         map = torch.zeros_like(logits).int().scatter(1, indices, 1).bool()
         scores = logits * map
+        if self.training:
+            tokens_per_expert = map.sum(dim=0)
+
+            save_to_tokens_per_expert_tracker(
+                "sinkhorn_tokens_per_expert",
+                tokens_per_expert,
+                self.layer_number,
+                self.config.num_layers,
+                reduce_group=parallel_state.get_data_parallel_group())
         return scores, map
 
     def compute_routing_scores_for_aux_loss(self, logits: torch.Tensor) -> torch.Tensor:
@@ -247,6 +256,12 @@ class TopKRouter(Router):
             probs = self.apply_load_balancing_loss(
                 activation=probs, load_balancing_loss_func=aux_loss_func
             )
+            save_to_tokens_per_expert_tracker(
+                "aux_tokens_per_expert",
+                tokens_per_expert,
+                self.layer_number,
+                self.config.num_layers,
+                reduce_group=parallel_state.get_data_parallel_group())
         return probs, routing_map
 
     def seq_aux_loss_load_balancing(self, logits: torch.Tensor, bsz: int, seq_length: int):
@@ -292,6 +307,12 @@ class TopKRouter(Router):
                 activation=probs, load_balancing_loss_func=aux_loss_func
             )
 
+            save_to_tokens_per_expert_tracker(
+                "seq_aux_tokens_per_expert",
+                tokens_per_expert,
+                self.layer_number,
+                self.config.num_layers,
+                reduce_group=parallel_state.get_data_parallel_group())
         return probs, routing_map
 
     def global_batch_loss_load_balancing(self, logits: torch.Tensor):
