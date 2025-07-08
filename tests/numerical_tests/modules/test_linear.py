f6ebad8641c7f75bf3c5a54a5b0d8bcaaf1c39c3
Ziyue Yang <ziyyang@microsoft.com>
2025-07-08 11:13:02 +0000

    threshold

---
+++ tests/numerical_tests/modules/test_linear.py
@@ -0,0 +1,57 @@
+import pytest
+import torch
+
+from types import SimpleNamespace
+
+from megatron.core.extensions.transformer_engine import TELinear
+from megatron.core.transformer import TransformerConfig
+from megatron.training.global_vars import set_args
+from tests.numerical_tests.modules.test_module import TestModule
+
+
+class TestTELinear(TestModule):
+    """
+    Test TELinear
+    """
+
+    @pytest.mark.parametrize("seq", [4096])
+    @pytest.mark.parametrize("mbs", [1])
+    @pytest.mark.parametrize("input_output_size", [[5120, 2160]])
+    @pytest.mark.parametrize("parallel_mode", ['duplicated'])
+    @pytest.mark.parametrize("dtype_str", ['bf16'])
+    @pytest.mark.parametrize("lr", [1e-3])
+    @pytest.mark.parametrize("steps", [10])
+    def test_te_linear(self, seq, mbs, input_output_size, parallel_mode, dtype_str, lr, steps, request):
+        if dtype_str == 'bf16':
+            dtype = torch.bfloat16
+        else:
+            raise ValueError(f'Unsupported dtype {dtype_str}')
+
+        args = SimpleNamespace()
+        args.bf16 = (dtype == torch.bfloat16)
+        set_args(args)
+        config = TransformerConfig(
+            num_attention_heads=1,
+            num_layers=1,
+            bf16=args.bf16
+        )
+
+        input_size, output_size = input_output_size
+        model = TELinear(
+            input_size=input_size,
+            output_size=output_size,
+            parallel_mode=parallel_mode,
+            config=config,
+            init_method=config.init_method,
+            bias=False,
+            skip_bias_add=False,
+            skip_weight_param_allocation=False,
+        )
+        self.setup_model_and_optimizer(config, model, lr)
+
+        self.run_test_loop(
+            input_shape=(seq, mbs, input_size),
+            input_dtype=dtype,
+            steps=steps,
+            request=request,
+        )
---
+++ tests/numerical_tests/modules/test_module.py
@@ -0,0 +1,62 @@
+import os
+import pytest
+import torch
+
+from megatron.core.distributed import DistributedDataParallel, DistributedDataParallelConfig
+from megatron.core.optimizer import OptimizerConfig, get_megatron_optimizer
+from megatron.core.tensor_parallel.random import model_parallel_cuda_manual_seed
+from megatron.core.transformer.module import Float16Module
+from tests.numerical_tests.modules.test_utilities import Utils
+
+
+class TestModule:
+    """
+    Test general modules
+    """
+
+    def setup_method(self, method):
+        Utils.initialize_distributed()
+        Utils.initialize_model_parallel()
+        seed = 42
+        torch.manual_seed(seed)
+        model_parallel_cuda_manual_seed(seed)
+
+    def teardown_method(self, method):
+        Utils.destroy_model_parallel()
+
+    def setup_model_and_optimizer(self, config, model, lr):
+        model = model.cuda()
+        if config.bf16:
+            model = Float16Module(config, model)
+        ddp_config = DistributedDataParallelConfig(use_distributed_optimizer=True)
+        model = DistributedDataParallel(config, ddp_config, model)
+        optimizer_config = OptimizerConfig(
+            optimizer='adam',
+            bf16=config.bf16,
+            use_distributed_optimizer=True,
+            lr=lr,
+            clip_grad=0.0
+        )
+        optimizer = get_megatron_optimizer(optimizer_config, [model])
+        self.model = model
+        self.optimizer = optimizer
+
+    def get_save_tag(self, request):
+        result_dir = request.config.getoption('--result-dir')
+        file_tag = request.node.nodeid[request.node.nodeid.find(self.__class__.__name__):]
+        file_tag = file_tag.replace('::', '-').replace('[', '-').replace(']', '-')
+        file_name = f'{file_tag}.pt'
+        return os.path.join(result_dir, file_name)
+
+    def run_test_loop(self, input_shape, input_dtype, steps, request):
+        for step in range(steps):
+            input_tensor = torch.randn(input_shape, dtype=input_dtype).cuda()
+            output_tensor, _ = self.model(input_tensor)
+            loss = output_tensor.mean()
+            loss.backward()
+            self.optimizer.step()
+        state_dict = {
+            'params': self.optimizer.get_parameters(),
+            'grads': self.optimizer.get_main_grads_for_grad_norm(),
+        }
+        torch.save(state_dict, self.get_save_tag(request))
