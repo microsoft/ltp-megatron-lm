3c186a09aa84f0b91b16233e4de65ae9d9db2c7f
Ziyue Yang <ziyyang@microsoft.com>
2025-07-10 10:14:02 +0000

    change design

---
+++ tests/numerical_tests/modules/test_attention.py
@@ -0,0 +1,100 @@
+import pytest
+import torch
+
+from types import SimpleNamespace
+from megatron.core.extensions.transformer_engine import (
+    TEDotProductAttention,
+    TELayerNormColumnParallelLinear,
+    TENorm,
+    TERowParallelLinear,
+)
+from megatron.core.transformer import TransformerConfig
+from megatron.core.transformer.attention import SelfAttention, SelfAttentionSubmodules
+from megatron.core.transformer.enums import AttnMaskType
+from megatron.core.transformer.identity_op import IdentityOp
+from megatron.core.transformer.spec_utils import ModuleSpec, build_module
+from megatron.training.global_vars import set_args
+from tests.numerical_tests.modules.test_module import TestModule
+
+
+class TestSelfAttention(TestModule):
+    """
+    Test SelfAttention
+    """
+
+    @pytest.mark.parametrize('inputs_kv', [
+        {
+            'seq_length': 4096,
+            'micro_batch_size': 1,
+        },
+    ])
+    @pytest.mark.parametrize('config_kv', [
+        {
+            'bf16': True,
+            'num_layers': 1,
+            'hidden_size': 5120,
+            'num_attention_heads': 64,
+            'num_query_groups': 8,
+            'kv_channels': 128,
+            'qk_layernorm': True,
+            'attention_dropout': 0.0,
+            'hidden_dropout': 0.0,
+        },
+    ])
+    def test_te_dot_product_attention(self, inputs_kv, config_kv, request):
+        config = TransformerConfig(**config_kv)
+
+        if config.bf16:
+            dtype = torch.bfloat16
+        else:
+            dtype = torch.float32
+
+        module_spec = ModuleSpec(
+            module=SelfAttention,
+            params={'attn_mask_type': AttnMaskType.causal},
+            submodules=SelfAttentionSubmodules(
+                linear_qkv=TELayerNormColumnParallelLinear,
+                core_attention=TEDotProductAttention,
+                linear_proj=TERowParallelLinear,
+                q_layernorm=TENorm if config.qk_layernorm else IdentityOp,
+                k_layernorm=TENorm if config.qk_layernorm else IdentityOp,
+            ),
+        )
+        model = build_module(
+            module_spec,
+            config=config,
+            layer_number=0,
+        )
+        model, optimizer = self.setup_model_and_optimizer(config, model)
+
+        inputs = (
+            torch.randn(
+                (
+                    inputs_kv['seq_length'],
+                    inputs_kv['micro_batch_size'],
+                    config.hidden_size
+                ),
+                dtype=dtype
+            ).cuda(),
+            torch.triu(
+                torch.ones(
+                    (
+                        inputs_kv['seq_length'],
+                        inputs_kv['seq_length']
+                    ),
+                    dtype=torch.bool
+                ), diagonal=1
+            ).unsqueeze(0).unsqueeze(0).cuda(),
+        )
+        output, bias = model(*inputs)
+        merged_output = output + bias
+        loss = merged_output.mean()
+        loss.backward()
+        optimizer.step()
+
+        self.save_output(
+            [merged_output],
+            optimizer.get_parameters(),
+            optimizer.get_main_grads_for_grad_norm(),
+            request
+        )
--- tests/numerical_tests/modules/test_module.py
+++ tests/numerical_tests/modules/test_module.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 import torch
 
 from megatron.core.distributed import DistributedDataParallel, DistributedDataParallelConfig
@@ -16,15 +15,18 @@ class TestModule:
 
     def setup_method(self, method):
         Utils.initialize_distributed()
-        Utils.initialize_model_parallel()
+        self.setup_parallelism()
         seed = 42
         torch.manual_seed(seed)
         model_parallel_cuda_manual_seed(seed)
 
+    def setup_parallelism(self):
+        Utils.initialize_model_parallel()
+
     def teardown_method(self, method):
         Utils.destroy_model_parallel()
 
-    def setup_model_and_optimizer(self, config, model, lr):
+    def setup_model_and_optimizer(self, config, model):
         model = model.cuda()
         if config.bf16:
             model = Float16Module(config, model)
@@ -34,21 +36,21 @@ class TestModule:
             optimizer='adam',
             bf16=config.bf16,
             use_distributed_optimizer=True,
-            lr=lr,
+            lr=1e-3,
             clip_grad=0.0
         )
         optimizer = get_megatron_optimizer(optimizer_config, [model])
-        self.model = model
-        self.optimizer = optimizer
+        return model, optimizer
 
-    def save_output(self, model_outputs, model_params, model_grads, request):
+    def save_output(self, outputs, params, grads, request):
         output_dict = {
-            'model_outputs': model_outputs,
-            'model_params': model_params,
-            'model_grads': model_grads,
+            'outputs': outputs,
+            'params': params,
+            'grads': grads,
         }
         result_dir = request.config.getoption('--result-dir')
         file_tag = request.node.nodeid[request.node.nodeid.find(self.__class__.__name__):]
         file_tag = file_tag.replace('::', '-').replace('[', '-').replace(']', '')
-        file_name = f'{file_tag}.pt'
+        rank = torch.distributed.get_rank()
+        file_name = f'{file_tag}-rank-{rank}.pt'
         torch.save(output_dict, os.path.join(result_dir, file_name))
